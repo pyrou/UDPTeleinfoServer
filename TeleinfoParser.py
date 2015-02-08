4e13209cfe0fe3ae05bbe66222e23f5ef31b86d2
Michael Hurni <michael.hurni@gmail.com>
2015-02-08 13:04:44 +0100

    Integer cast for some datagram key

--- TeleinfoParser.py
+++ TeleinfoParser.py
@@ -2,6 +2,12 @@
 
 class TeleinfoParser:
 
+	integerKeys = [
+		'ISOUSC', 'BASE', 'HCHC', 'HCHP', 'EJPHN', 'EJPHPM',
+		'BBRHCJB', 'BBRHPJB', 'BBRHCJW', 'BBRHPJW', 'BBRHCJR', 'BBRHPJR',
+		'PEJP', 'IINST', 'IINST1', 'IINST2', 'IINST3', 'ADPS', 'IMAX',
+		'IMAX1', 'IMAX2', 'IMAX3', 'PAPP', 'ADIR1', 'ADIR2', 'ADIR3' ]
+
 	def checksum (self, label, value):
 		"""Return the checksum of a value and label"""
 		sum = 32
@@ -12,6 +18,6 @@ class TeleinfoParser:
 		"""Read and verify Teleinfo datagram and return it as a dict"""
 		trames = [trame.split(" ") for trame in message.strip("\r\n\x03\x02").split("\r\n")]
 		return dict([
-			[trame[0],trame[1]] for trame in trames
+			[trame[0], int(trame[1]) if trame[0] in self.integerKeys else trame[1]] for trame in trames
 			if (len(trame) == 3) and (self.checksum(trame[0],trame[1]) == trame[2])
 			])
